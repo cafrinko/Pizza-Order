6c89305e51f42a1e6ec5cebab37ec795ec18aa9d
Catherine Karra <catherine.karra@gmail.com>
2016-04-07 22:03:45 -0700

    adds pizza size and toppings checkboxes, and creates pizza constructor

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function Pizza {
+  this.size = size;
+  this.baseToppings = baseToppings;
+  this.fancyToppings = fancyToppings;
+}
+
+Pizza.prototype.Cost = function() {
+  this.size
+}
+
+$(document).ready(function() {
+  $("form.pizzaChar").submit(function(event) {
+
+  });
+});
