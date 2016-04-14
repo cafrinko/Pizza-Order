b4b2722c401554977a20075da676a2f283ada69d
Catherine Karra <catherine.karra@gmail.com>
2016-04-14 11:46:02 -0700

    calculates cost of pizza plus toppings and outputs correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,43 @@
-// function Size(small, medium, large) {
-//   this.small = small;
-//   this.medium = medium;
-//   this.large = large;
-// }
-
 function Pizza(size, toppings) {
   this.size = size;
-  this.toppings = [];
+  this.toppings = toppings;
 }
 
 Pizza.prototype.cost = function() {
 //size of pizza cost + #of toppings above 2* 1
   var total = 0;
   if (this.size === "small") {
-    total = 10;
+    total += 10;
   } else if (this.size === "medium") {
-    total = 15;
+    total += 15;
   } else {
-    total = 20;
+    total += 20;
   }
-
-  for (i = 0; i <= this.toppings.length; i++) {
-    if (this.toppings.length <= 1) {
-      total
+  if (this.toppings.length <= 2) {
+      total += 0;
     } else {
-      total += this.toppings.length * 1;
+      //add the number of toppings over 2 to the existing total
+      total = total + this.toppings.length - 2;
+      console.log(total);
     }
-  }
-  return total
-}
+  return total;
+};
 
 $(document).ready(function() {
   $("form.pizza").submit(function(event) {
     event.preventDefault();
-    var size = $("#size").val();
+    var size = $("input:radio[name=size]:checked").val();
+    console.log(size);
 
     var toppings = [];
-    $(".checkbox-inline:checked").each(function() {
+    $("input[type=checkbox]:checked").each(function() {
         toppings.push($(this).val());
-      });
+        console.log(toppings);
+    });
 
     var pizza = new Pizza(size, toppings);
     var cost = pizza.cost();
-
+    console.log(pizza);
     $(".total").text(cost);
     $(".output").show();
   });
